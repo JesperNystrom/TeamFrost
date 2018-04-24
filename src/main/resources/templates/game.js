a14231cb99026c1b73826fb503a559c8cb11d91d
Jesper Nyström <jesper.nystrom@live.se>
2018-04-24 13:42:03 +0200

    Add attack sprites, fall and jump modified

--- src/main/resources/templates/game.js
+++ src/main/resources/templates/game.js
@@ -16,9 +16,8 @@ var config = {
     }
 };
 
-var playerStatesEnum = {"attacking":true, "moving":true, "idle":true}
-Object.freeze(playerStatesEnum);
-var jumper;
+var states;
+var fallBuffert;
 var cursors;
 var layer;
 var tileset;
@@ -34,14 +33,15 @@ function preload() {
         { frameWidth: 128, frameHeight: 140 });
     this.load.spritesheet('playerHeavyAttack', '../sprites/PlayerHeavyAtk.png',
         { frameWidth: 217, frameHeight: 187 });
-    this.load.spritesheet('playerLightAttack', '../sprites/PlayerLightAtk.png',
-        { frameWidth: 217, frameHeight: 187 });
+    this.load.spritesheet('playerLightAttack', '../sprites/PlayerLightAttack.png',
+        { frameWidth: 216, frameHeight: 135 });
     this.load.spritesheet('playerGlide', '../sprites/PlayerGlide.png',
         { frameWidth: 199, frameHeight: 168 });
     this.load.spritesheet('playerJump', '../sprites/PlayerJump.png',
         { frameWidth: 128, frameHeight: 140 });
     this.load.spritesheet('playerFall', '../sprites/PlayerFall.png',
         { frameWidth: 128, frameHeight: 150 });
+        
 
 
     this.load.image('dirtGrass', '../sprites/ground.png');
@@ -54,7 +54,7 @@ function preload() {
 function create() {
 
     //FloorCounter
-    jumper = 25;
+    fallBuffert = 25;
 
     //Creating Map
     map = this.make.tilemap({ key: 'map', tileWidth: 64, tileHeight: 64 });
@@ -71,6 +71,7 @@ function create() {
     cursors = game.input.keyboard.createCursorKeys();
     key_X = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
     key_Z = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+    key_jump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
     //Camera
     this.cameras.main.setSize(1137, 640);
@@ -92,14 +93,14 @@ function create() {
         key: 'heavyAttack',
         frames: this.anims.generateFrameNumbers('playerHeavyAttack', { start: 0, end: 9 }),
         frameRate: 20,
-        repeat: 0
+        repeat: 1
     });
 
     this.anims.create({
         key: 'run',
         frames: this.anims.generateFrameNumbers('playerRun', { start: 0, end: 19 }),
         frameRate: 20,
-        repeat: -19
+        repeat: 1
     });
 
     this.anims.create({
@@ -132,50 +133,83 @@ function create() {
 function update() {
 
     if (!player.body.onFloor()) {
-        jumper--;
+        fallBuffert--;
     } else if (player.body.onFloor()) {
-        jumper = 25;
+        fallBuffert = 25;
     }
 
-    //Plays animation
-    if (cursors.down.isDown && jumper > 0) {
+
+    //Plays animation'
+    switch (states) {
+        case 'gliding':
         player.anims.play('glide', true);
-        if (player.flipX == false) {
-            player.setVelocityX(400)
-        } else {
+        break;
+        case 'run' :
+        player.anims.play('run', true);
+        break;
+        case 'lightAttack':
+        player.anims.play('lightAttack', true);
+        break;
+        case 'heavyAttack':
+        player.anims.play('heavyAttack', true);
+        break;
+        case 'jump':
+        player.anims.play('jump', true);
+        break;
+        case 'fall':
+        player.anims.play('fall', true);
+        break;
+        default:
+        player.anims.play('idle', true);
+    }
+
+
+
+    if (cursors.down.isDown && fallBuffert > 0) {
+        states = 'gliding';
+        if (player.flipX) {
             player.setVelocityX(-400)
+        } else {
+            player.setVelocityX(400)
         }
+
     } else if (cursors.left.isDown) {
         player.setVelocityX(-260);
 
         if (player.body.onFloor())
-            player.anims.play('run', true);
+        states = 'run';
 
         player.flipX = true;
     } else if (cursors.right.isDown) {
         player.setVelocityX(260);
         player.flipX = false;
         if (player.body.onFloor()){
-            player.anims.play('run', true);
+            states = 'run';
         }
+
     } else {
         player.setVelocityX(0);
+        states = 4;
+    }
+    if (key_Z.isDown && !cursors.down.isDown){
+        states = 'lightAttack';
     }
 
-    if (key_X.isDown){
-        player.anims.play('heavyAttack', true);
+    if (key_X.isDown && !cursors.down.isDown){
+        states = 'heavyAttack';
     }
 
-    if (cursors.up.isDown && player.body.onFloor()) {
+    if (key_jump.isDown && player.body.onFloor()) {
         player.setVelocityY(-400);
         if (player.body.velocity.y < 0 && player.body.velocity.x !=0)
-            player.anims.play('jump', true);
-        jumper = 0;
+            states = 'jump';
+        fallBuffert = 0;
     }
-    if (player.body.velocity.y > 200) {
-        player.anims.play('fall', true);
+
+    if (player.body.velocity.y > 0 && !key_X.isDown && !key_Z.isDown) {
+       states = 'fall';
     }
+
     if(player.body.velocity.x == 0 && player.body.velocity.y == 0){
-        player.anims.play('idle', true);
     }
 }
