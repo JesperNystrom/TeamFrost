8eb79ad3b39f1bb0d45a1bee026f01643083c3f5
unknown <robin.backman@hotmail.se>
2018-04-27 09:59:32 +0200

    add all levels but 4 and boss to WorldMap

--- src/main/resources/sprites/allTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="allTiles" tilewidth="64" tileheight="64" tilecount="15" columns="5">
- <image source="../../Pictures/allTiles.png" width="320" height="192"/>
-</tileset>
