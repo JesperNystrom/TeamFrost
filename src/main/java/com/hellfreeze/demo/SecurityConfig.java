b511cb2f466cc6f9722c4467b519acedd353222b
Sofie Westerdahl <algotson@gmail.com>
2018-05-02 15:31:18 +0200

    Fix font in css

--- src/main/java/com/hellfreeze/demo/SecurityConfig.java
+++ src/main/java/com/hellfreeze/demo/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/index", "/registration", "/success","/","login", "/css/**").permitAll()
+                .antMatchers("/index", "/registration", "/success","/","login", "/css/**", "/Fonts/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
